450033fa3550d3c81a25c4a4a082624dddcf3cd7
Aaron.Gibson <codebyexample@gmail.com>
2021-07-06 19:40:52 +0100

    Add files via upload

---
+++ LoadTesting.Api/Controllers/TestingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoadTesting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestingController : ControllerBase
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok("Bom");
+        }
+    }
+}
